f91db86f827f091115f75bd3d70ca1623f3ec9ef
David M <davidm8624@gmail.com>
2022-02-14 13:52:49 -0700

    first commit

---
+++ pages/components/common/ToDoItem.jsx
@@ -0,0 +1,21 @@
+import { Message } from 'semantic-ui-react'
+
+const ToDoItem = ({ todoObject }) => {
+
+  const users = ['sixty', 'nine', '69', 'jo', 'joo', 'jooo', 'joooo', 'joh', 'bil', 'bill'];
+
+  const { userId, title, completed, id } = todoObject;
+
+  return (
+    <Message
+      content={title}
+      color={completed ? 'green' : 'red'}
+      header={users[userId - 1]}
+      icon={{
+        name: completed ? 'check square outline' : 'square outline',
+      }}
+    />
+  );
+}
+
+export default ToDoItem
